47cedc3fef33670cd7701869ca6a589a406db805
John L <j42558080+1@gmail.com>
2025-08-14 12:12:46 -0400

    Expand time range of EQDSK loads

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import numpy as np
 
 from model import CGTS
 
-rampup_times = np.linspace(20.0, 80.0, 5)
-flattop_times = np.linspace(120.0, 130.0, 5)
+rampup_times = np.linspace(10.0, 90.0, 5)
+flattop_times = np.linspace(110.0, 140.0, 5)
 times = np.r_[rampup_times, flattop_times]
 
 g_arr1 = [f'ramp3/iter_i={i}.eqdsk' for i in range(5)]
-g_arr2 = [f'Lmode_ex.eqdsk'] * 5
+# g_arr2 = ['Lmode_ex.eqdsk'] * 3 + ['ramp3/iter_i=2'] * 2
+g_arr2 = ['Lmode_ex.eqdsk'] * 5
 g_arr = np.r_[g_arr1, g_arr2]
 
 mysim = CGTS(times, g_arr)
