d686f66a27dab1d1a7bf7e3554309300d753fbc1
John L <j42558080+1@gmail.com>
2025-08-28 14:27:55 -0400

    Update ex4 to LH-transition at 80s

--- example4.py
+++ example4.py
@@ -2,11 +2,11 @@ import numpy as np
 
 from model import CGTS
 
-# Reproduciton of Capser (2013)
+# Reproduciton of Capser (2014)
 
 # Set timesteps
-rampup_times = np.linspace(10.0, 90.0, 10)
-flattop_times = np.linspace(110.0, 490.0, 10)
+rampup_times = np.linspace(10.0, 70.0, 10)
+flattop_times = np.linspace(90.0, 490.0, 10)
 rampdown_times = np.linspace(510.0, 590.0, 10)
 times = np.r_[rampup_times, flattop_times, rampdown_times]
 
@@ -17,14 +17,17 @@ g_arr_rampdown = g_arr_rampup[::-1]
 g_arr = np.r_[g_arr_rampup, g_arr_flattop, g_arr_rampdown]
 
 # Set current
-ip = {0: 4.0E6, 10: 4.0E6, 100: 15.0E6, 500: 15.0E6, 590: 4.0E6, 600: 4.0E6}
+ip = {0: 4.0E6, 10: 4.0E6, 80: 15.0E6, 500: 15.0E6, 590: 4.0E6, 600: 4.0E6}
 
 # Set heating
-nbi_powers = {0: 0, 99: 0, 100: 40.0E6, 500: 40.0E6, 501: 0, 600: 0}
+nbi_powers = {0: 0, 79: 0, 80: 40.0E6, 500: 40.0E6, 501: 0, 600: 0}
 
 # Set pedestals
-T_i_ped = {0: 0.5, 100: 0.5, 105: 4.5, 495: 4.5, 500: 0.5}
-T_e_ped = {0: 0.5, 100: 0.5, 105: 4.5, 495: 4.5, 500: 0.5}
+T_i_ped = {0: 0.5, 80: 0.5, 85: 4.5, 495: 4.5, 500: 0.5}
+T_e_ped = {0: 0.5, 80: 0.5, 85: 4.5, 495: 4.5, 500: 0.5}
+
+# Set Z_eff
+# override_z_eff = {'plasma_composition': {'Z_eff': 1.8}}
 
 mysim = CGTS(600, times, g_arr)
 mysim.initialize_gs('ITER_mesh.h5', vsc='VS')
