c1da3ba9ca998ed7c0715974e402e2c12be4fe5d
John L <j42558080+1@gmail.com>
2025-10-29 12:01:20 -0400

    Set t_init=0.5

--- diiid.py
+++ diiid.py
@@ -17,10 +17,8 @@ for fname in eqdsk_names:
     eqdsk.append(f'163303/eqs_safe/{fname}')
 
 # eqdsk1 = ['eqdsk/iter_i=0.eqdsk'] * len(eqdsk)
-times = times[1:]
-eqdsk = eqdsk[1:]
 
-mysim = CGTS(1.0, 5.0, times, eqdsk, dt=1.0E-2)
+mysim = CGTS(0.5, 5.0, times, eqdsk, dt=1.0E-2)
 mysim.initialize_gs('163303/DIIID_mesh.h5')
 
 target_currents = {
@@ -87,39 +85,61 @@ T_e_right_bc = {}
 T_i_right_bc = {}
 n_e_right_bc = {}
 
-for path in eqdsk:
-    name = path.split('/')[-1]
-    name = name.split('.')[0]
-    t_tag = int(name.split('-')[1][1:])
-    time = float(t_tag) / 1e3
-    t_tag = f'{t_tag:05d}'
-    path = f'163303/profs/p163303.{t_tag}'
+ne = {}
+Te = {}
+Ti = {}
+
+prof_names = sorted(os.listdir('163303/profs'))
+prof_times = []
+for fname in prof_names:
+    if 'OMFIT' in fname or 'DS_Store' in fname:
+        continue
+    _, t = fname.split('.')
+    t = float(t) / 1e3
+    prof_times.append(t)
+
+    path = f'163303/profs/{fname}'
     data = read_pfile(path)
 
     psi_space = sorted(data['te(KeV)'].keys())
+    rho_space = [np.sqrt(psi) for psi in psi_space]
     te_space = [data['te(KeV)'][psi] for psi in psi_space]
-    te_ped = np.interp(0.95, psi_space, te_space)
+    Te[t] = {rho: te_space[i] for i, rho in enumerate(rho_space)}
+    te_ped = np.interp(0.95, rho_space, te_space)
 
     psi_space = sorted(data['ti(KeV)'].keys())
+    rho_space = [np.sqrt(psi) for psi in psi_space]
     ti_space = [data['ti(KeV)'][psi] for psi in psi_space]
-    ti_ped = np.interp(0.95, psi_space, ti_space)
+    Ti[t] = {rho: ti_space[i] for i, rho in enumerate(rho_space)}
+    ti_ped = np.interp(0.95, rho_space, ti_space)
 
     psi_space = sorted(data['ne(10^20/m^3)'].keys())
+    rho_space = [np.sqrt(psi) for psi in psi_space]
     ne_space = [data['ne(10^20/m^3)'][psi] * 1e20 for psi in psi_space]
-    ne_ped = np.interp(0.95, psi_space, ne_space)
+    ne[t] = {rho: ne_space[i] for i, rho in enumerate(rho_space)} # Use rho coords
+    ne_ped = np.interp(0.95, rho_space, ne_space)
+    nbar[t] = np.mean(ne_space)
 
-    nbar[time] = np.mean(ne_space)
+    T_e_ped[t] = te_ped
+    T_i_ped[t] = ti_ped
+    n_e_ped[t] = ne_ped
 
-    T_e_ped[time] = te_ped
-    T_i_ped[time] = ti_ped
-    n_e_ped[time] = ne_ped
-
-    T_e_right_bc[time] = te_space[-1]
-    T_i_right_bc[time] = ti_space[-1]
-    n_e_right_bc[time] = ne_space[-1]
+    T_e_right_bc[t] = te_space[-1]
+    T_i_right_bc[t] = ti_space[-1]
+    n_e_right_bc[t] = ne_space[-1]
 
 mysim.set_nbar(nbar)
 mysim.set_pedestal(T_e_ped=T_e_ped, T_i_ped=T_i_ped, n_e_ped=n_e_ped)
 mysim.set_right_bc(Te_right_bc=T_e_right_bc, Ti_right_bc=T_i_right_bc, ne_right_bc=n_e_right_bc)
 
-mysim.fly(graph=False)
+# print('hello world')
+# print(sorted(Te.keys()))
+startup_times = [t for t in prof_times if t <= times[0]]
+Te_init = {t: Te[t] for t in startup_times}
+Ti_init = {t: Ti[t] for t in startup_times}
+mysim.set_density(ne)
+mysim.set_Te(Te)
+mysim.set_Ti(Ti)
+mysim.set_evolve(density=False)
+
+mysim.fly(save_states=True, graph=False)
