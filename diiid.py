472a8a7d434e1c21de91691f5906583fa842e5a3
John L <j42558080+1@gmail.com>
2025-11-03 12:35:27 -0500

    Get basic DIIID working (no density)

--- diiid.py
+++ diiid.py
@@ -56,15 +56,16 @@ mysim.set_coil_reg(targets=target_currents)
 
 ip_f = open('163303/ip.json')
 ip = json.load(ip_f)
-ip = {t: ip['data'][i] for i, t in enumerate(ip['time'])}
+ip = {t / 1e3: ip['data'][i] for i, t in enumerate(ip['time'])}
 mysim.set_Ip(ip)
 
 ech_f = open('163303/pech.json')
 ech = json.load(ech_f)
-ech = {t: ech['data'][i] for i, t in enumerate(ech['time'])}
+ech = {t / 1e3: ech['data'][i] for i, t in enumerate(ech['time'])}
+# ech = {t: 1e5 for i, t in enumerate(ech['time'])}
 inj_f = open('163303/pinj.json')
 inj = json.load(inj_f)
-inj = {t: inj['data'][i] for i, t in enumerate(inj['time'])}
+inj = {t / 1e3: inj['data'][i] for i, t in enumerate(inj['time'])}
 mysim.set_heating(eccd=ech, eccd_loc=0.1, nbi=inj, nbi_loc=0.1)
 
 def read_pfile(path):
@@ -137,17 +138,40 @@ for fname in prof_names:
     T_i_right_bc[t] = ti_space[-1]
     n_e_right_bc[t] = ne_space[-1]
 
-mysim.set_nbar(nbar)
+# Smooth Pedestals
+Te_ped_list = [T_e_ped[t] for t in prof_times]
+Ti_ped_list = [T_i_ped[t] for t in prof_times]
+ne_ped_list = [n_e_ped[t] for t in prof_times]
+
+Te_bc_list = [T_e_right_bc[t] for t in prof_times]
+Ti_bc_list = [T_i_right_bc[t] for t in prof_times]
+ne_bc_list = [n_e_right_bc[t] for t in prof_times]
+
+nbar_list = [nbar[t] for t in prof_times]
+
+t_inc = np.linspace(2.0, 2.5, 5000)
+for t in t_inc:
+    T_e_ped[t] = np.interp(t, prof_times, Te_ped_list)
+    T_i_ped[t] = np.interp(t, prof_times, Ti_ped_list)
+    n_e_ped[t] = np.interp(t, prof_times, ne_ped_list)
+
+    T_e_right_bc[t] = np.interp(t, prof_times, Te_bc_list)
+    T_i_right_bc[t] = np.interp(t, prof_times, Ti_bc_list)
+    n_e_right_bc[t] = np.interp(t, prof_times, ne_bc_list)
+
+    nbar[t] = np.interp(t, prof_times, nbar_list)
+
+# mysim.set_nbar(nbar)
 mysim.set_pedestal(T_e_ped=T_e_ped, T_i_ped=T_i_ped, n_e_ped=n_e_ped)
 mysim.set_right_bc(Te_right_bc=T_e_right_bc, Ti_right_bc=T_i_right_bc, ne_right_bc=n_e_right_bc)
 
 Te_init = {1.02: Te[1.02]}
 Ti_init = {1.02: Ti[1.02]}
 ne_init = {1.02: ne[1.02]}
-# mysim.set_density(ne)
+mysim.set_density(ne)
 mysim.set_Te(Te_init)
 mysim.set_Ti(Ti_init)
-mysim.set_density(ne_init)
-# mysim.set_evolve(density=False)
+# mysim.set_density(ne_init)
+mysim.set_evolve(density=False)
 
 mysim.fly(save_states=True, graph=False)
